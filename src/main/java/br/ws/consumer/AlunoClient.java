b73a0877eaf66804845f6a9eff4541882dbcd037
Alfredo Oliveira <alfredo.coj@gmail.com>
2017-04-24 21:33:44 -0300

    criado novo metodo: addAluno.

--- src/main/java/br/ws/consumer/AlunoClient.java
+++ src/main/java/br/ws/consumer/AlunoClient.java
@@ -1,7 +1,10 @@
 package br.ws.consumer;
 
+import br.ws.consumer.entities.Aluno;
 import br.ws.consumer.wsdl.GetAlunoRequest;
 import br.ws.consumer.wsdl.GetAlunoResponse;
+import br.ws.consumer.wsdl.PostAlunoRequest;
+import br.ws.consumer.wsdl.PostAlunoResponse;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
@@ -15,6 +18,20 @@ public class AlunoClient extends WebServiceGatewaySupport{
                 "http://localhost:8080/ws/alunos.wsdl",
 				request,
 				new SoapActionCallback("http://localhost:8080/ws/getAlunoResponse"));
+
+		return response;
+	}
+
+	public PostAlunoResponse addAluno(Aluno aluno) {
+		PostAlunoRequest request = new PostAlunoRequest();
+		request.setAlunoName( aluno.getAlunoName() );
+		request.setAlunoIdade( aluno.getAlunoIdade() );
+		request.setAlunoClasse( aluno.getAlunoClasse() );
+
+		PostAlunoResponse response = (PostAlunoResponse) getWebServiceTemplate().marshalSendAndReceive(
+				"http://localhost:8080/ws/alunos.wsdl",
+				request,
+				new SoapActionCallback("http://localhost:8080/ws/postAlunoResponse"));
 
 		return response;
 	}
